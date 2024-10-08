88a86d586fb7ee283e5d3bda2602549089079990
Jenna Ray <jennastingray@gmail.com>
2024-10-08 13:42:07 -0600

    added sprite class, background class, spritesheet class, image folders + animation and movement code for main character in main game code

--- game.py
+++ game.py
@@ -1,35 +1,87 @@
 import pygame
+import player
+import background
+#animation code from coding with russ tutorial
+#https://www.youtube.com/watch?v=nXOVcOBqFwM&t=33s
 
 pygame.init()
 
-screen_width = 800
-screen_height = 600
+SCREEN_WIDTH = 600
+SCREEN_HEIGHT = 600
 
-screen = pygame.display.set_mode((screen_width, screen_height))
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Sprite")
 
-player = pygame.Rect((300, 250, 50, 50))
+bg = background.Background("images/scenes/room.png", 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT)
+
+player = player.Player(400, 250, 0, 0, "images/sprites/chameleon-sprite.png")
+
+action = player.get_action()
+last_update = pygame.time.get_ticks()
+animation_cooldown = 110
+frame = player.get_frame()
+
 
 run = True
 while run:
-
+    #update background
     screen.fill((0, 0, 0))
+    bg.draw(screen)
 
-    pygame.draw.rect(screen, (255, 0, 0), player)
+    #update animation
+    current_time = pygame.time.get_ticks()
+    if current_time - last_update >= animation_cooldown:
+        player.set_frame(frame + 1)
+        frame = player.get_frame()
+        last_update = current_time
+        if frame >= len(player.get_animation()):
+            player.set_frame(0)
+            frame = player.get_frame()
 
-    key = pygame.key.get_pressed()
-    if key[pygame.K_a] == True:
-        player.move_ip(-1, 0)
-    if key[pygame.K_d] == True:
-        player.move_ip(1, 0)
-    if key[pygame.K_w] == True:
-        player.move_ip(0, -1)
-    if key[pygame.K_s] == True:
-        player.move_ip(0, 1)
-
+    #show frame image
+    player.draw(screen)
+    #event handler
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            run = False
-
+            run = False 
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_a:
+                player.set_action(1)
+                action = player.get_action()
+                player.set_frame(0)
+                frame = player.get_frame()
+                player.move_left()
+            if event.key == pygame.K_d:
+                player.set_action(2)
+                action = player.get_action()
+                player.set_frame(0)
+                frame = player.get_frame()
+                player.move_right()
+            if event.key == pygame.K_w:
+                player.set_action(4)
+                action = player.get_action()
+                player.set_frame(0)
+                frame = player.get_frame()
+                player.move_up()
+            if event.key == pygame.K_s:
+                player.set_action(3)
+                action = player.get_action()
+                player.set_frame(0)
+                frame = player.get_frame()
+                player.move_down()
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_a:
+                player.set_action(0)
+                action = player.get_action()
+                player.set_frame(0)
+                frame = player.get_frame()
+            if event.key == pygame.K_d or event.key == pygame.K_w or event.key == pygame.K_s or event.key == pygame.K_a:
+                player.set_action(0)
+                action = player.get_action()
+                player.set_frame(0)
+                frame = player.get_frame()
+            player.stand_still()
+    player.update()
     pygame.display.update()
 
 pygame.quit()
