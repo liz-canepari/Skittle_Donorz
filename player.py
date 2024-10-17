21f5638dfab34c374e111aa8de562e4c97c24cb5
Jenna Ray <jennastingray@gmail.com>
2024-10-17 14:54:54 -0600

    comments added to player class

--- player.py
+++ player.py
@@ -4,18 +4,22 @@ import spritesheet
 class Player():
     #Animation code from Coding with Russ tutorial
     #https://www.youtube.com/watch?v=nXOVcOBqFwM&t=33s
-    animation_list = []
-    animation_steps = []
-    current_frame = 0
-    current_action = 0
-    position = [0, 0]
-    velocity = [0, 0]
-    SPEED = .50
+
+    animation_list = [] #will hold all animation frames
+    animation_steps = [] #used to set up animation frames for each action--number coordinates with number of frames in each animation
+    current_frame = 0 #current animation frame for character display
+    current_action = 0 
+    position = [0, 0] #coordinates of character on screen
+    velocity = [0, 0] #controls how quickly character moves and in what direction 
+    SPEED = .50 #controls how quickly character moves --- used in when changing velocity
     
     def __init__(self, x, y, velocity_x, velocity_y, image_path):
+
+        #load sprite sheet
         sprite_sheet_image = pygame.image.load(image_path).convert_alpha()
         sprite_sheet = spritesheet.SpriteSheet(sprite_sheet_image)
-
+       
+        #load animation frames
         frame_0 = sprite_sheet.get_image(0, 0, 32)
         animation_list = []
         animation_steps = [3, 4, 4, 4, 4]
@@ -33,13 +37,7 @@ class Player():
         self.image = self.animation_list[0][0]
         self.velocity = [velocity_x, velocity_y]
         self.position = [x, y]
-
-    def set_frame(self, frame):
-        self.current_frame=frame
-
-    def set_action(self, action):
-        self.current_action=action
-
+#-----------------------------------------------getters---------------------------------------
     def get_frame(self):
         return self.current_frame
     
@@ -57,43 +55,63 @@ class Player():
     
     def get_y(self):
         return self.position[1]
+#-----------------------------------------------setters---------------------------------------
+    def set_frame(self, frame):
+        self.current_frame=frame
+
+    def set_action(self, action):
+        self.current_action=action
+
     
+    def set_velocity_x(self, x, y):
+        self.velocity = [x, y]
+
+    #Updates position of the player using velocity
     def update(self):
         self.position[0] += self.velocity[0]
         self.position[1] += self.velocity[1]
         
+    #put character on screen
     def draw(self, surface):
         surface.blit(self.get_animation_frame(), self.position)
 
+#---------------------------------------------------------movement functions----------------------------------
     def move_left(self):
+        # change velocity x value
         self.velocity[0] = -(self.SPEED)
+        #set animation to left facing walking animation
         self.set_action(1)
         self.set_frame(0)
     
     def move_right(self):
+        #change velocity x value
         self.velocity[0] = (self.SPEED)
+        #set animation to right facing walking animation
         self.set_action(2)
         self.set_frame(0)
     
     def move_up(self):
+        #change velocity y value
         self.velocity[1] = -(self.SPEED)
+        #set animation to backwards facing walking animation
         self.set_action(4)
         self.set_frame(0)
     
     def move_down(self):
+        #change velocity y value
         self.velocity[1] = (self.SPEED)
+        #set to move forward facing walking animation
         self.set_action(3)
         self.set_frame(0)
     
+    #Character goes back to idle and stops moving
     def stand_still(self):
+        #change velocity
         self.velocity[0] = 0
         self.velocity[1] = 0
+        #set to idle animation
         self.set_action(0)
         self.set_frame(0)
-
-    def set_velocity_x(self, x, y):
-        
-        self.velocity = [x, y]
     
     # Calculate the distance between the player and the object. -Porter
     def player_is_near(self, obj_position, threshold=40):
